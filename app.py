5b8e5ffef969a02bcaf026300ae2d442159c4def
Zach Gordon <zachgordon25@gmail.com>
2023-07-17 13:15:01 -0400

    I cleaned up app.py. added upload.html

--- app.py
+++ app.py
@@ -3,10 +3,7 @@ import cv2
 import numpy as np
 import os
 import tempfile
-
 from skeleton.extractKimiaEDF import generate_skeleton
-
-# from skeleton.BlumMedialAxis import BlumMedialAxis
 
 
 app = Flask(__name__)
@@ -18,117 +15,71 @@ def get_pixel_coordinates(image, scale_x, scale_y):
         for x in range(image.shape[1]):
             if np.any(image[y, x] != [255, 255, 255]):
                 coords.append((x * scale_x, y * scale_y))
-    app.logger.info("get_pixel_coordinates")
     return coords
 
 
 def process_image(file_path):
-    # Define the target height
     target_height = 200
-
-    # Load the image
     image = cv2.imread(file_path)
-
     aspect_ratio = image.shape[1] / image.shape[0]
     target_width = int(target_height * aspect_ratio)
 
-    # Calculate scale factors for x and y coordinates
     scale_x = target_width / image.shape[1]
     scale_y = target_height / image.shape[0]
 
-    # Resize the image while keeping the aspect ratio
     resized_image = cv2.resize(image, (target_width, target_height))
-
-    # Convert the image to grayscale
     gray_image = cv2.cvtColor(resized_image, cv2.COLOR_BGR2GRAY)
-
-    # Apply edge detection
     edges = cv2.Canny(gray_image, threshold1=30, threshold2=100)
 
-    # Find contours
     contours, _ = cv2.findContours(edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-
-    # Create a white image with the same dimensions as the resized image
     white_image = np.ones_like(resized_image) * 255
 
     # Draw blue contours on the white image
     cv2.drawContours(white_image, contours, -1, (255, 0, 0), 1)
 
-    # Get the coordinates of all non-white pixels in the image
     coordinates = get_pixel_coordinates(white_image, scale_x, scale_y)
-
-    # Format contours as list of strings
     height = edges.shape[0]
-    contour_strings = []
-    for contour in contours:
-        for point in contour:
-            contour_string = "{:.7e} {:.7e}".format(
-                float(point[0][0]), float(height - point[0][1])
-            )
-            contour_strings.append(contour_string)
+    contour_strings = [
+        "{:.7e} {:.7e}".format(float(point[0][0]), float(height - point[0][1]))
+        for contour in contours
+        for point in contour
+    ]
 
-    # Define the output as a dictionary
     output = {"coordinates": coordinates, "contour_strings": contour_strings}
-
-    # Return the output
-    app.logger.info("process_image")
     return output
 
 
 @app.route("/")
 def home():
-    app.logger.info("home")
     return render_template("upload.html")
 
 
 @app.route("/upload", methods=["POST"])
 def upload_image():
-    if "file" not in request.files:
-        return "No file part"
-
     file = request.files["file"]
-    if file.filename is None:
-        return "No file uploaded", 400
 
-    # Now you can use file.filename
-    file_path = os.path.join(tempfile.gettempdir(), file.filename)
-    if file:
-        # Save the file temporarily
+    if file and file.filename:
         file_path = os.path.join(tempfile.gettempdir(), file.filename)
         file.save(file_path)
 
-        # TODO: You'll need to decide how to handle the output of the processing.
-        # You can access the coordinates with output['coordinates']
-        # You can also access the contour strings with output['contour_strings']
+        # You can access the coordinates with output["coordinates"]
+        # You can also access the contour strings with output["contour_strings"]
 
-        # Now call your image processing function
         output = process_image(file_path)
 
         filename = os.path.splitext(file.filename)[0]
         skeleton_img_base64 = generate_skeleton(output["contour_strings"], filename)
 
-        img_file_name = os.path.basename(skeleton_img_base64)
+        return render_template(
+            "result.html", skeleton_img_base64=skeleton_img_base64, filename=filename
+        )
 
-        app.logger.info("upload_image")
-        return f"""
-        <h1>Image uploaded and processed</h1>
-        <img src="data:image/png;base64,{skeleton_img_base64}" alt="Skeleton image">
-        <a href="data:image/png;base64,{skeleton_img_base64}" download="{filename}-skeleton.png">Download skeleton image</a>
-        """
-    return "No file uploaded"
+    return "No file was uploaded. Please upload a file.", 400
 
 
 @app.route("/uploads/<filename>")
 def send_uploaded_file(filename):
-    app.logger.info("send_uploaded_file")
     return send_from_directory("skeleton", filename)
-
-
-@app.route("/view_image")
-def view_image():
-    app.logger.info("view_image")
-    # TODO: add your code to display the image here
-    return "View image page"
 
 
 if __name__ == "__main__":
